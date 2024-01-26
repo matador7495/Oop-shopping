3ebbcd52730b8a6857d9f427c13dc25acef612b3
mr.nobody <id4acc2014@protonmail.com>
2024-01-26 22:43:34 +0330

    get node cartList & totalPrice
    create instance cart and add parameter to productsInstance

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+import Cart from "./models/Cart.js";
 import Products from "./models/Products.js";
 import { fetchData } from "./utils/httpReq.js";
 
 const productsNode = document.getElementById("products");
+const cartListNode = document.getElementById("cart-list");
+const totalPriceNode = document.getElementById("total-price").querySelector("span");
 
 async function render() {
   const productsData = await fetchData();
-  const productsInstance = new Products(productsNode, productsData);
+
+  const cartInstance = new Cart(cartListNode, totalPriceNode);
+  const productsInstance = new Products(productsNode, productsData, cartInstance);
+
   productsInstance.showProducts();
 }
 

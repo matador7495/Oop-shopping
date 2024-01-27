31dd3a4e1b848cdfeb83935ca17b11d05d4a0804
mr.nobody <id4acc2014@protonmail.com>
2024-01-27 11:21:52 +0330

    create method createCard
    create method productImg productInfo productControl

--- models/Cart.js
+++ models/Cart.js
@@ -17,8 +17,55 @@ class Cart {
     });
   }
 
-  createCard(product, quantity) {
-    console.log(product);
+  createCard(data, quantity) {
+    const cardEle = document.createElement("div");
+
+    const imgEle = this.productImg(data);
+    const infoEle = this.productInfo(data);
+    const controlEle = this.productControl(data, quantity);
+
+    cardEle.innerHTML = imgEle;
+    cardEle.innerHTML += infoEle;
+    cardEle.innerHTML += controlEle;
+
+    this.cartList.appendChild(cardEle);
+  }
+
+  productImg(data) {
+    const { image, alt } = data;
+
+    const imgJSX = `<img src=${image} alt=${alt} />`;
+
+    return imgJSX;
+  }
+
+  productInfo(data) {
+    const { name, price } = data;
+
+    const infoJSX = `
+      <div>
+        <h4>${name}</h4>
+        <p>${price}</p>
+      </div>
+    `;
+
+    return infoJSX;
+  }
+
+  productControl(data, quantity) {
+    const { id } = data;
+    const controlJSX = `
+      <div>
+        <div>
+          <button data-id=${id} > - </button>
+          <span>${quantity}</span>
+          <button data-id=${id} > + </button>
+        </div>
+        <button data-id=${id} >Remove></button>
+      </div>
+    `;
+
+    return controlJSX;
   }
 }
 
